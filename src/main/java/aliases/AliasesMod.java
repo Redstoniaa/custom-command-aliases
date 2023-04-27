e987a5e8948c7bdc907967b975ae964383019d59
Redstonia <redstonia1234@gmail.com>
2023-04-27 17:50:22 +1200

    whoops; forgot to actually commit the files, here they are!

---
+++ src/main/java/aliases/AliasesMod.java
@@ -0,0 +1,15 @@
+package aliases;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AliasesMod implements ModInitializer {
+	public static final String MOD_ID = "custom-command-aliases";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		//LOGGER.info("Hello Fabric world!");
+	}
+}
