9fc8a90b4d260ddc481f7245ba08364cbb3a8ad5
Redstonia <redstonia1234@gmail.com>
2023-05-08 17:20:09 +1200

    finally found the cause of the unshortenable code (hint: my stupidity)

--- src/main/java/aliases/command/RegisterAliasCommands.java
+++ src/main/java/aliases/command/RegisterAliasCommands.java
@@ -2,13 +2,11 @@ package aliases.command;
 
 import aliases.AliasDeclaration;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.server.command.ServerCommandSource;
 
 import static aliases.Config.aliases;
-
-import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
+import static net.minecraft.server.command.CommandManager.*;
 
 public class RegisterAliasCommands {
     public static void registerAliases(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -16,9 +14,7 @@ public class RegisterAliasCommands {
             CommandNode<ServerCommandSource> commandNode = dispatcher.getRoot().getChild(aliasGroup.command);
             if (commandNode != null) {
                 for (String alias : aliasGroup.aliases) {
-                    LiteralArgumentBuilder<ServerCommandSource> builder = literal(alias);
-                    dispatcher.register(builder.redirect(commandNode));
-                    // can be shortened but compiler doesn't like for some reason
+                    dispatcher.register(literal(alias).redirect(commandNode));
                 }
             }
         }
